4f5534c3839ece3d3f4bf61c232b1a3f0d4f5c18
Michael Caton <warehouse.architect@gmail.com>
2016-06-22 14:33:30 -0500

    Apply migrations to Customer and get customer data from DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,28 +9,30 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Name = "John Smith", Id = 1},
-                new Customer {Name = "Mary Williams", Id = 2}
-            };
+        private ApplicationDbContext _context;
 
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
 
 
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
         // GET: Customers
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
